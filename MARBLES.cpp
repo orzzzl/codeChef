6e6eaa3adea4a94b82ce4b30e931b1df3a1adc70
Zeleng Zhuang <zhuangzeleng19920731@gmail.com>
2015-06-26 01:26:26 -0400

    dont want to say anything.

---
+++ MARBLES.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int T;
+
+int main () {
+    cin >> T;
+    while (T --) {
+	int  a, b;
+	cin >> a >> b; b--;
+	int k = (a - b - 1) > b ? b : a - b - 1;
+        long long ans = 1;
+	for (int kk = 1; kk <= k; kk ++)
+	    ans = ans * (a - kk) / kk;
+	cout << ans << endl;
+    }
+    return 0;
+}
